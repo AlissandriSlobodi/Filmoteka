4aba2b0a1718f8403a1d7077c9a8d928ef2eb942
AlissandriSlobodi <alissandri4@gmail.com>
2024-04-01 17:23:44 +0300

    created new files

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import * as renderingMovieCards from './js/rendering-movie-cards';
+import * as searchByKeyword from './js/search-by-keyword';
+import * as openModal from './js/open-and-close-modal';
